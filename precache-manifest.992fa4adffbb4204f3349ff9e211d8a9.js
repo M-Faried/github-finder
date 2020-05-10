ee44365c21d97faaa422decdb63840b5116616cb
M Faried <m.a.faried@gmail.com>
2020-05-10 14:22:01 +0200

    Updates

---
+++ precache-manifest.992fa4adffbb4204f3349ff9e211d8a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32ad88db1aa11edf3a373c0b8477b14b",
+    "url": "/github-finder/index.html"
+  },
+  {
+    "revision": "79172b6c0ff7c6f13158",
+    "url": "/github-finder/static/css/main.94f8da69.chunk.css"
+  },
+  {
+    "revision": "a6aef137d15ba7f4d38b",
+    "url": "/github-finder/static/js/2.9ae917fa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-finder/static/js/2.9ae917fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79172b6c0ff7c6f13158",
+    "url": "/github-finder/static/js/main.0a0bbaf1.chunk.js"
+  },
+  {
+    "revision": "a7114b735a442fd4e553",
+    "url": "/github-finder/static/js/runtime-main.ffe1c599.js"
+  }
+]);
